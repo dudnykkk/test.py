bed7dca80c4a47bb63ad7569bb4c6ea4e22e87f3
Ihor_Dudnyk <dudnyk1005@gmail.com>
2026-03-04 21:41:08 +0100

    update code

--- lesson0/for.py
+++ lesson0/for.py
@@ -1,20 +1,43 @@
-numbers = [1,2,3,4]
-for n in numbers:
-    print(n)
+# numbers = [1,2,3,4]
+# for n in numbers:
+#     print(n)
 
-for i in range(6):
-    print(i)
-
-
-for i in range(2, 6):
-    print(i)
+# for i in range(6):
+#     print(i)
 
 
-numbers = [5, 12, 8, 20, 3]
+# for i in range(2, 6):
+#     print(i)
 
-for n in numbers:
-    if n > 10:
-        print(n)
+
+# numbers = [5, 12, 8, 20, 3]
+
+# for n in numbers:
+#     if n > 10:
+#         print(n)
 
 
 #завтра повторить и использовать больше с if elif else
+
+total = 0
+for i in range(1, 101):
+    total = total + i
+print(total)
+# расчет чисел идет суммирования к каждому числу
+
+# for day in range(1, 6):
+#     print("Рабочий день:", day)
+
+# for i in range(1, 11):
+#     print(f"Число: {i} " )
+
+total = 0
+
+for i in range(1, 11):
+    total = total + i
+
+print(total)
+
+for i in range(1, 11):
+    result = i * 2
+    print(result)
